4ce6e1495cc0e143e5469b5702e25c2916bdcf10
Vasyl Protsukovych <protcykovich@gmail.com>
2024-04-13 21:25:55 +0200

    Implemented task 1, 2, 3

---
+++ js/task-1.js
@@ -0,0 +1,34 @@
+function isEnoughCapacity(products, containerSize) {
+    let currentSize = 0;
+
+    for (const productName in products) {
+        const productSize = products[productName];
+
+        currentSize += productSize;
+
+        if (currentSize > containerSize) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log('Task 1:\n');
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+console.log('\n');
---
+++ js/task-2.js
@@ -0,0 +1,46 @@
+function calcAverageCalories(days) {
+    let totalCalories = 0;
+
+    if (!days.length) {
+        return totalCalories;
+    }
+
+    for (const dayItem of days) {
+        const currentCalories = dayItem.calories;
+        
+        totalCalories += currentCalories;
+    }
+    
+    return totalCalories / Object.keys(days).length;
+}
+
+console.log('Task 2:\n');
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
+
+console.log('\n');
